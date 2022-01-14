a296763e6cd0b4ad0623c39f175a2af0b2fe4077
THuix <tom.huix@polytechnique.edu>
2022-01-14 16:17:55 +0100

    classic nn

---
+++ exps/run_nn.py
@@ -0,0 +1,21 @@
+from run_exp import main
+import argparse
+
+lr = 1e-1
+regime = "nn"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('dataset')
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if args.dataset == '0':
+        print('CIFAR10 DATASET')
+        dataset_name = "CIFAR10"
+    else:
+        print('MNIST dataset')
+        dataset_name = 'MNIST'
+
+    range_N = [5000]
+    project_name = f'bnn_bbb_regime_nn_{dataset_name}'
+    for N in range_N:
+        main(N, lr, None, None, 'nn', project_name, dataset_name)
