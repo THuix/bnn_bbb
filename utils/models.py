692a315522806e5fd9c1e82ff6cbc519df5ac9bc
THuix <tom.huix@polytechnique.edu>
2022-01-15 12:10:19 +0100

    momentum

--- utils/models.py
+++ utils/models.py
@@ -295,7 +295,7 @@ class CNN(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.lr, momentum=0.9)
         return optimizer
 
 
