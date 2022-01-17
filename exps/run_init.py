7db3b4ac9ac415fb14ac9a0a142bc1660a549df7
THuix <tom.huix@polytechnique.edu>
2022-01-17 09:49:02 +0100

    t

--- exps/run_init.py
+++ exps/run_init.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         dataset_name = 'BOSTON'
 
     range_N = range(100, 1000, 100)
-    alpha = 2000 / 60000
+    alpha = 10000 / 60000
 
     project_name = f'new_bnn_bbb_regime_{regime}_init_{dataset_name}'
     for N in range_N:
