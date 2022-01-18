24e1351edaa3f464a210c7bfbc153a188a2b5397
THuix <tom.huix@polytechnique.edu>
2022-01-18 19:52:47 +0100

    t

--- utils/distributions.py
+++ utils/distributions.py
@@ -5,11 +5,13 @@ from security import check
 eps = 1e-6
 
 def norm_log_prob(w, m, s):
-        if type(s) == torch.Tensor:
-            s[s==0.] = eps
-        else:
-            if s == 0.:
-                s = eps
-        log_prob = torch.distributions.normal.Normal(m, s).log_prob(w)
+        # if type(s) == torch.Tensor:
+        #     s[s==0.] = eps
+        # else:
+        #     if s == 0.:
+        #         s = eps
+        # log_prob = torch.distributions.normal.Normal(m, s).log_prob(w)
+        p = torch.exp(- (w - m)**2 / (2 * s**2)) / (s * np.sqrt(2 * np.pi))
+        log_prob = torch.log(p)
         check(log_prob, items=(w, m, s))
         return log_prob.sum()
