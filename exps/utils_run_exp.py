906de47b6ddebc49a6ec9b938e93df7660a107c3
THuix <tom.huix@polytechnique.edu>
2022-01-18 20:07:42 +0100

    t

--- exps/utils_run_exp.py
+++ exps/utils_run_exp.py
@@ -99,7 +99,7 @@ def get_exp_name(train_params, model_params):
 
 def get_trainer(nb_epochs, wandb_logger, lr_monitor, exp_name):
     if torch.cuda.is_available():
-        trainer = pl.Trainer(gpus=-1, max_epochs=nb_epochs, logger= wandb_logger, callbacks=[lr_monitor], weights_save_path = exp_name, strategy='dp')
+        trainer = pl.Trainer(gpus=-1, max_epochs=nb_epochs, logger= wandb_logger, callbacks=[lr_monitor], weights_save_path = exp_name, strategy='ddp')
     else:
         trainer = pl.Trainer(max_epochs=nb_epochs, logger= wandb_logger)
     return trainer
