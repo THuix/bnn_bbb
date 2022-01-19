ed94ecfa8a7ae81266453d37d6456a2328d0008f
THuix <tom.huix@polytechnique.edu>
2022-01-19 19:58:44 +0100

    t

--- exps/utils_run_exp.py
+++ exps/utils_run_exp.py
@@ -10,7 +10,7 @@ from models import Linear_regime_1, Linear_regime_2, Linear_regime_3
 from models import Conv_regime_1, Conv_regime_3
 from models import VGG_regime_1, VGG_regime_3
 from models import Resnet_regime_1, Resnet_regime_3
-from models_vgg import VGG_classic
+from model_vgg import VGG_classic
 from torchvision import transforms
 from torchvision.datasets import MNIST, CIFAR10
 from torch.utils.data import DataLoader
