44e459dbe89c7a5c8e999714e899f730465c8b1e
THuix <tom.huix@polytechnique.edu>
2022-01-19 19:28:56 +0100

    t

--- utils/utils_models.py
+++ utils/utils_models.py
@@ -140,8 +140,8 @@ class BNN(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.train_params['lr'])
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 7 , gamma=0.1, verbose=True)
-        return [optimizer], [scheduler]
+        #scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 7 , gamma=0.1, verbose=True)
+        return [optimizer]#, [scheduler]
 
 
 
