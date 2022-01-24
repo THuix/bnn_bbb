bc31a1abc7a9cc089caa7861e951b676721501fc
THuix <tom.huix@polytechnique.edu>
2022-01-24 09:16:38 +0100

    t

--- utils/utils_models.py
+++ utils/utils_models.py
@@ -126,11 +126,15 @@ class BNN(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         loss, logs = self.step(batch, batch_idx)
         self.log_dict({f"train_{k}": v for k, v in logs.items()}, sync_dist=True)
+        self.accuracy.reset()
+        self.ECE.reset()
         return loss
 
     def validation_step(self, batch, batch_idx):
         loss, logs = self.step(batch, batch_idx)
         self.log_dict({f"val_{k}": v for k, v in logs.items()}, sync_dist=True)
+        self.accuracy.reset()
+        self.ECE.reset()
         return loss
 
     def plot_hist(self, values, title, name):
@@ -147,6 +151,8 @@ class BNN(pl.LightningModule):
     def test_step(self, batch, batch_idx):
         loss, logs = self.step(batch, batch_idx)
         self.log_dict({f"test_{k}": v for k, v in logs.items()}, sync_dist=True)
+        self.accuracy.reset()
+        self.ECE.reset()
         return loss
 
     def test_epoch_end(self, output):
