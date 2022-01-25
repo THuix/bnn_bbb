a9fa443ea6b371c04b801431858c56fc62bc6910
THuix <tom.huix@polytechnique.edu>
2022-01-25 07:31:37 +0100

    t

--- exps/utils_run_exp.py
+++ exps/utils_run_exp.py
@@ -43,7 +43,7 @@ def load_cifar(batch_size, num_works):
 
 def limit_size(dataset, size):
     indexes = np.random.choice(range(0, dataset.dataset.__len__()), size=size, replace=False).astype(int)
-    print(indexes)
+    print(indexes, dataset.dataset.targets)
     dataset.dataset.targets = dataset.dataset.targets[indexes]
     dataset.dataset.data = dataset.dataset.data[indexes] 
     return dataset  
