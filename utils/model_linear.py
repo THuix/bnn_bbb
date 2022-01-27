47001e42263aa29261c32222abfdd83bd283a533
THuix <tom.huix@polytechnique.edu>
2022-01-27 18:00:06 +0100

    t

--- utils/model_linear.py
+++ utils/model_linear.py
@@ -145,7 +145,7 @@ class NN(pl.LightningModule):
     def configure_optimizers(self):
         #optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=wd)
         #scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 50 , gamma=0.1, verbose=True)
-        optimizer = torch.optim.SGD(self.parameters(), lr=self.lr, momentum=0.9, weight_decay=self.model_params['alpha'])
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.lr, momentum=0.9, weight_decay=1e-3 * self.train_params['alpha'])
         scheduler = torch.optim.lr_scheduler.MultiplicativeLR(optimizer, lr_lambda=self.lambda_fct)
         return [optimizer], [scheduler]
 
